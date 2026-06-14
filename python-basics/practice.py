dbc3f5f45768c05396147f0dd865a811bfc5ba79
Ayusha Gautam <agautam.connect@gmail.com>
2026-06-13 21:30:12 -0400

    add while loop nested loop and sum and product function

--- python-basics/practice.py
+++ python-basics/practice.py
@@ -29,3 +29,24 @@ print(third_power)
 
 odds = [x for x in range(20) if x % 2 != 0 ]
 print(odds)
+
+# While loop- count from 1 to 10 
+i = 1
+while i <= 10: 
+    print (i)
+    i += 1 
+
+# Nested loop- multiplication table 
+for i in range (1, 11): #the outer loop is like the hour hand 
+    for j in range (1, 11): #the inner loop is like the minute hand
+        print(f"{i} x {j} = {i * j}") 
+
+# Function - sum and product 
+def sum_and_product(a, b): 
+    total = a + b 
+    product = a * b 
+    return total , product
+
+result = sum_and_product(5, 5)
+print(result)
+
